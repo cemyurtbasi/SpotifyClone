3eb40a153ed03e1693a7d98bb5537b486e698bdf
cemyurtbasi <cemyurtbasi@gmail.com>
2021-03-28 19:58:54 +0300

    İnitial Commit, frontend defaults

---
+++ frontend/src/Components/playList/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlayList = (props) => {
+    return (
+        <div className="playList">
+            
+        </div>
+    );
+};
+
+export default PlayList;
---
+++ frontend/src/Router/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Login from "../Components/memberShip/login";
+
+const Router = () => {
+    return <Login />;
+};
+
+export default Router;
