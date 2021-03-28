56e743851253276d9d2c54218f5dd48a058bba67
cemyurtbasi <cemyurtbasi@gmail.com>
2021-03-28 22:29:35 +0300

    done

--- frontend/src/Router/index.js
+++ frontend/src/Router/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from "react";
 import Login from "../Components/memberShip/login";
+import PlayList from "../Components/playList";
+
+const code = new URLSearchParams(window.location.search).get("code");
 
 const Router = () => {
-    return <Login />;
+  return code ? <PlayList code={code} /> : <Login />;
 };
 
 export default Router;
